e7832d59382d58968aa55fbac96109c7c985c9f0
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-05-06 14:21:29 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,15 @@ export default function Home() {
 <main className={styles.main}>
      
       <div className={styles.hero}>
-            <h2 className={styles.holy}>THE HOLY</h2>
+            <h2 className={styles.holy}>THE WORD</h2>
                 <h1 className={styles.bible}>
-                    <a href="https://the-holy-bible.vercel.app/" target="_blank">BIBLE</a>
+                    <a href="https://the-holy-bible.vercel.app/" target="_blank">OF GOD</a>
                 </h1>
         
         <h1 className={styles.title}>
          PSALMS
         </h1>
-        </div>
+        
         <div  className={styles.image}>
        <Image
                                 src="/139943824-biblical-vector-illustration-series-moses-received-the-ten-commandments.webp"
@@ -168,7 +168,7 @@ Strike all my enemies on the jaw;
               </div>
 
 
-
+</div>
           </div>
 </main>
         <p className={styles.description}> 
