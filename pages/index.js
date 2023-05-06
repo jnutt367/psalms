75192150e3483ebddcc3d6852c1864d7716d2201
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-05-06 13:21:32 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,27 @@ export default function Home() {
         
        
         <div className={styles.grid}>
-       
+        <div className={styles.card}>
+            <h2 className={styles.chapter}>PSALM 1 &darr;</h2>
+            <p>( A Psalm of David )
+Blessed is the one
+    who does not walk in step with the wicked
+or stand in the way that sinners take
+    or sit in the company of mockers,
+ but whose delight is in the law of the Lord,
+    and who meditates on his law day and night.
+ That person is like a tree planted by streams of water,
+    which yields its fruit in season
+and whose leaf does not wither—
+    whatever they do prospers.
+    Not so the wicked!
+    They are like chaff
+    that the wind blows away.
+    Therefore the wicked will not stand in the judgment,
+    nor sinners in the assembly of the righteous.
+    For the Lord watches over the way of the righteous,
+    but the way of the wicked leads to destruction.</p>
+              </div>
 
           </div>
 </main>
